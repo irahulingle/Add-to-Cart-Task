d683f18b519c61f4dca352f943e597133df4ee97
Rahul INgle <er.rahulingle96@gmail.com>
2025-06-21 18:09:07 +0530

    Fixed Error

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-
+import Cart from "./pages/Cart";
 import { CartProvider } from "./context/CartContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
